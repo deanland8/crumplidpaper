5326934ca7b8609fd0d18fe3370a977c29bb9730
deanland8 <85014798+deanland8@users.noreply.github.com>
2021-07-11 13:05:57 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var world;
+var ball;
+var ground;
+var binleft;
+var binright;
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	var ball_options={
+		isStatic: false,
+		restitution: 0.3,
+		friction: 0,
+		density:1.2	
+	   };
+
+ball= Bodies.circle(260,600,20,ball_options); 
+World.add(world,ball);
+
+ground= new Ground(width/2,670,width,20);
+  binleft= new Ground(1100,600,20,120);
+  binright= new Ground(1350,600,20,120);
+  Engine.run(engine);
+}
+
+
+function draw() {
+  
+  background(0);
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x, ball.position.y,20);
+
+  binright.display();
+  binleft.display();
+  ground.display();
+ drawSprites();
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+  
+	}
+}
+
+
